6c4db180052bba3a1a5895928306e416513d07de
Asami Doi <doiasami1219@gmail.com>
2017-06-13 10:13:58 +0900

    modify: reverse polish notation

--- improved_calculator.py
+++ improved_calculator.py
@@ -1,6 +1,18 @@
+from enum import Enum
+
+
+class Operator(Enum):
+    PLUS = 0
+    MINUS = 0
+    MULTIPY = 1
+    DEVIDE = 1
+    BRACKET = 2
+
+
 class Calculator(object):
-    
-    def readNumber(line, index):
+
+
+    def readNumber(self, line, index):
         number = 0
         while index < len(line) and line[index].isdigit():
             number = number * 10 + int(line[index])
@@ -12,12 +24,31 @@ class Calculator(object):
                 number += int(line[index]) * keta
                 keta *= 0.1
                 index += 1
-        return token, index
+        return number, index
 
-    def evaluate(tokens):
+
+    def convert(self, line):
+        operators = []
+        tokens = []
+        index = 0
+        while index < len(line):
+            if line[index].isdigit():
+                token, index = self.readNumber(line, index)
+                tokens.append(token)
+            else:
+                operators.append(line[index])
+                index += 1
+        tokens.extend(operators)
+        return tokens
+
+
+    def evaluate(self, line):
+        tokens = self.convert(line)
+        answer = tokens
         return answer
 
 
 if __name__ == '__main__':
     calc = Calculator()
-    print(calc.evaluate("1+2+3"))
+    print("1.5+2+3.5")
+    print(calc.evaluate("1.5+2+3.5"))
